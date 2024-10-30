d69ae84f492b73fa02e4de92f298c08a827e8e98
Bhenyowsky <benimor05@gmail.com>
2024-10-30 16:26:30 +0100

    JBM hazifeladatok

--- homework/find_a_string/find_a_string.py
+++ homework/find_a_string/find_a_string.py
@@ -1,3 +1,10 @@
-def find_a_string(original_str: str, substr: str) -> int:
-    """Write your code here"""
-    pass
+def find_a_string(original_str: str = "", substr: str = "") -> int:
+    if not original_str or not substr:
+        return 0
+
+    count = 0
+    for i in range(len(original_str) - len(substr) + 1):
+        if original_str[i:i + len(substr)] == substr:
+            count += 1
+    return count
+
